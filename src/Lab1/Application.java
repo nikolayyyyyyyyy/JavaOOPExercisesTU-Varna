1eab3f8e9b9c18fa6bc3b970b9b26bd46ad8a29b
Nikolay Nikolaev <cholyo999999@gmail.com>
2024-10-13 17:13:26 +0300

    Deleted folder

--- src/Lab1/Application.java
+++ src/Lab1/Application.java
@@ -1,8 +1,7 @@
 package Lab1;
 import Lab1.Iterfaces.IItem;
 import Lab1.Iterfaces.IItemArray;
-import Lab1.Models.Item;
-import Lab1.Models.ItemArray;
+
 import java.util.Scanner;
 
 public class Application {
